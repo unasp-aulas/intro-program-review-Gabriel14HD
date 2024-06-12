9b7c2d28be2383e58af226c1951da813aaffb119
Gabriel14HD <157375226+Gabriel14HD@users.noreply.github.com>
2024-06-12 19:59:51 -0300

    Update problem_2.py

---
+++ problem_2.py
@@ -0,0 +1,12 @@
+def main(ultimo):
+    return list(range(1, ultimo + 1))
+
+def soma(main):
+    return sum(main)
+
+ultimo = 10
+
+#lista = main(ultimo)
+#valor = soma(lista)
+#print(lista)
+#print(valor)
